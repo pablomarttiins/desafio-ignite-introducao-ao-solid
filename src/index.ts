d9ff65e20b9ceff9fd2644db11170be1d21c33ef
Pablo Martins <pablo.martins@hotmail.com.br>
2021-06-06 09:20:41 -0300

    Inclusão documentação Swagger

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
 import express from "express";
+import sweggerUI from 'swagger-ui-express';
 
 import { usersRoutes } from "./routes/users.routes";
 
+import swaggerFile from '../swagger.json';
+
 const app = express();
+
 
 app.use(express.json());
 
 app.use("/users", usersRoutes);
+
+app.use('/api-docs',sweggerUI.serve, sweggerUI.setup(swaggerFile));
 
 export { app };
